e0bdc20320ecb7b83313caa8ebc1105ee07cdfb4
Tobias Steffensen <Computer_X@outlook.dk>
2022-03-30 19:04:02 +0200

    Galllery page displaying grid and pictures

--- ViewModels/GalleryViewModel.cs
+++ ViewModels/GalleryViewModel.cs
@@ -2,24 +2,76 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace GalleryManegy.ViewModels
 {
     internal class GalleryViewModel : ViewModelBase
     {
         private readonly DatabaseContext Context;
+        private Size CurrentSize;
 
-        public ObservableCollection<ImageModel> Images { get; set; }
+        private double _rowCount;
+        public double RowCount { get { return _rowCount; } set
+            {
+                SetProperty(ref _rowCount, value);
+                OnChangeSize(CurrentSize);
+                SetImages();
+            }
+        }
+
+        private ObservableCollection<ObservableCollection<ImageModel>> _images;
+        public ObservableCollection<ObservableCollection<ImageModel>> Images { get => _images; set => SetProperty(ref _images, value); }
+
+        private double _rowWidth;
+        public double RowWidth { get { return _rowWidth; } set => SetProperty(ref _rowWidth, value); }
 
         public GalleryViewModel() : base("Gallery")
         {
             Context = new();
-            // Todo use loggedin user
-            Images = new(Context.Images.Where(i => i.User.Id == 1).ToList());
+
+            RowWidth = 100;
+            RowCount = 4;
+
+            SetImages();
         }
 
+        private void SetImages()
+        {
+            var output = new ObservableCollection<ObservableCollection<ImageModel>>();
+            // Todo use loggedin user
+            var images = Context.Images.Where(i => i.User.Id == 1).ToList();
+            var current = new ObservableCollection<ImageModel>();
+
+            for (int i = 0; i < images.Count; i++)
+            {
+                current.Add(images[i]);
+
+                if ((i + 1) % RowCount == 0)
+                {
+                    output.Add(current);
+                    current = new ObservableCollection<ImageModel>();
+                }
+            }
+
+            if (current.Count > 0)
+            {
+                output.Add(current);
+            }
+
+            Images = new(output);
+        }
+
+        public void OnChangeSize(Size newSize)
+        {
+            // - 5 to acount for scroolbar
+            RowWidth = newSize.Width / RowCount - 5;
+            CurrentSize = newSize;
+        }
     }
 }
--- Views/GalleryView.xaml.cs
+++ Views/GalleryView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GalleryManegy.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace GalleryManegy.Views
         public GalleryView()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            (DataContext as GalleryViewModel).OnChangeSize(e.NewSize);
         }
     }
 }
